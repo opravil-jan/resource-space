17902cb36366ce55ca73ca8e4128f9334d4c6d87
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-02-17 16:16:48 +0000

    FIX: with plugin activated all dropdown or checkbox fields were showing initial comma on view page

--- plugins/track_field_history/hooks/view.php
+++ plugins/track_field_history/hooks/view.php
@@ -12,10 +12,9 @@ function HookTrack_field_historyViewDisplay_field_modified_value($field)
 		$get_params = '?ref=' . $ref . '&field=' . $field['ref'] . '&field_title=' . $field['title'];
 
 		$field['value'] .= '<a href="' . $baseurl . '/plugins/track_field_history/pages/field_history_log.php' . $get_params . '" style="margin-left: 20px;">&gt;&nbsp;History</a>';
-
+		
+		return $field;
 	}
-
-	return $field;
 
 }
 
