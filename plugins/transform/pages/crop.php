725ca7e53a8ded8ce468677610185484b8129990
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-02-24 17:24:11 +0000

    FIX call to undefined function do_search issue

--- plugins/transform/pages/crop.php
+++ plugins/transform/pages/crop.php
@@ -3,6 +3,7 @@
 include_once "../../../include/db.php";
 include_once "../../../include/authenticate.php";
 include_once "../../../include/general.php";
+include_once "../../../include/search_functions.php";
 include_once "../../../include/resource_functions.php";
 include_once "../../../include/image_processing.php";
 
