061f4dfc2639ba2cea2731e8ac4e966e0582ba01
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-02-26 14:23:59 +0000

    FIX: exposed html for history link when using resource ids as search terms...caused by highlighting

--- plugins/track_field_history/hooks/view.php
+++ plugins/track_field_history/hooks/view.php
@@ -2,20 +2,17 @@
 
 #function HookTrack_field_historyViewHOOKNAME()
 
-function HookTrack_field_historyViewDisplay_field_modified_value($field)
-{
-
+function HookTrack_field_historyViewValue_mod_after_highlight($field,$value){
 	global $ref, $track_fields, $baseurl, $k;
 
 	if($k=='' && in_array($field['ref'], $track_fields)) {
-
+		
 		$get_params = '?ref=' . $ref . '&field=' . $field['ref'] . '&field_title=' . $field['title'];
 
-		$field['value'] .= '<a href="' . $baseurl . '/plugins/track_field_history/pages/field_history_log.php' . $get_params . '" style="margin-left: 20px;">&gt;&nbsp;History</a>';
+		$value.= '<a href="' . $baseurl . '/plugins/track_field_history/pages/field_history_log.php' . $get_params . '" style="margin-left: 20px;">&gt;&nbsp;History</a>';
 		
-		return $field;
+		return $value;
 	}
 
 }
-
 ?>
