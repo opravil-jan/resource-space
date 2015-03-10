9d15637f541cd646fcd8b6acf4ac41195e31a6e0
adoerfler <adoerfler@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-03-10 20:13:57 +0000

    CHG improved and simplified user request.
    * Moved auto_create_user_account(), and email_user_request() into
      user_request.php - no one else is using them. Removed unused globals
      from those functions, too.
    * Moved requirement check out of those functions. Instead, it's now done
      before, as it should. IOW instead of getting a "you didn't insert the
      spam code" message, and then a "I know this mail", before you'll see
      which fields you missed, you now get those first, and only get further
      when all fields are set.
    * Added "replacemainrequired" as counterpart to the "replacemain" hook;
      now you can truly change all fields as you wish.
    * Use $user_email instead of retrieving the field again, and again.

--- pages/user_request.php
+++ pages/user_request.php
@@ -9,19 +9,164 @@ $error_extra="";
 $user_email=getval("email","");
 hook("preuserrequest");
 
+if (!function_exists("auto_create_user_account")){
+function auto_create_user_account()
+	{
+	# Automatically creates a user account (which requires approval unless $auto_approve_accounts is true).
+	global $applicationname,$user_email,$baseurl,$email_notify,$lang,$user_account_auto_creation_usergroup,$registration_group_select,$auto_approve_accounts,$auto_approve_domains,$customContents;
+
+	# Work out which user group to set. Allow a hook to change this, if necessary.
+	$altgroup=hook("auto_approve_account_switch_group");
+	if ($altgroup!==false)
+		{
+		$usergroup=$altgroup;
+		}
+	else
+		{
+		$usergroup=$user_account_auto_creation_usergroup;
+		}
+
+	if ($registration_group_select)
+		{
+		$usergroup=getvalescaped("usergroup","",true);
+		# Check this is a valid selectable usergroup (should always be valid unless this is a hack attempt)
+		if (sql_value("select allow_registration_selection value from usergroup where ref='$usergroup'",0)!=1) {exit("Invalid user group selection");}
+		}
+
+	$username=escape_check(make_username(getval("name","")));
+
+	#check if account already exists
+	$check=sql_value("select email value from user where email = '$user_email'","");
+	if ($check!=""){return $lang["useremailalreadyexists"];}
+
+	# Prepare to create the user.
+	$email=trim(getvalescaped("email","")) ;
+	$password=make_password();
+
+	# Work out if we should automatically approve this account based on $auto_approve_accounts or $auto_approve_domains
+	$approve=false;
+	if ($auto_approve_accounts==true)
+		{
+		$approve=true;
+		}
+	elseif (count($auto_approve_domains)>0)
+		{
+		# Check e-mail domain.
+		foreach ($auto_approve_domains as $domain=>$set_usergroup)
+			{
+			// If a group is not specified the variables don't get set correctly so we need to correct this
+			if (is_numeric($domain)){$domain=$set_usergroup;$set_usergroup="";}
+			if (substr(strtolower($email),strlen($email)-strlen($domain)-1)==("@" . strtolower($domain)))
+				{
+				# E-mail domain match.
+				$approve=true;
+
+				# If user group is supplied, set this
+				if (is_numeric($set_usergroup)) {$usergroup=$set_usergroup;}
+				}
+			}
+		}
+
+	# Create the user
+	sql_query("insert into user (username,password,fullname,email,usergroup,comments,approved) values ('" . $username . "','" . $password . "','" . getvalescaped("name","") . "','" . $email . "','" . $usergroup . "','" . escape_check($customContents) . "'," . (($approve)?1:0) . ")");
+	$new=sql_insert_id();
+    hook("afteruserautocreated", "all",array("new"=>$new));
+	if ($approve)
+		{
+		# Auto approving, send mail direct to user
+		email_user_welcome($email,$username,$password,$usergroup);
+		}
+	else
+		{
+		# Not auto approving.
+		# Build a message to send to an admin notifying of unapproved user (same as email_user_request(),
+		# but also adds the new user name to the mail)
+		$message=$lang["userrequestnotification1"] . "\n\n" . $lang["name"] . ": " . getval("name","") . "\n\n" . $lang["email"] . ": " . getval("email","") . "\n\n" . $lang["comment"] . ": " . getval("userrequestcomment","") . "\n\n" . $lang["ipaddress"] . ": '" . $_SERVER["REMOTE_ADDR"] . "'\n\n" . $customContents . "\n\n" . $lang["userrequestnotification3"] . "\n$baseurl?u=" . $new;
+
+		send_mail($email_notify,$applicationname . ": " . $lang["requestuserlogin"] . " - " . getval("name",""),$message,"",$user_email,"","",getval("name",""));
+		}
+
+	return true;
+	}
+} //end function replace hook
+
+function email_user_request()
+	{
+	# E-mails the submitted user request form to the team.
+	global $applicationname,$user_email,$baseurl,$email_notify,$lang,$customContents;
+
+	# Build a message
+	$message=$lang["userrequestnotification1"] . "\n\n" . $lang["name"] . ": " . getval("name","") . "\n\n" . $lang["email"] . ": " . getval("email","") . "\n\n" . $lang["comment"] . ": " . getval("userrequestcomment","") . "\n\n" . $lang["ipaddress"] . ": '" . $_SERVER["REMOTE_ADDR"] . "'\n\n" . $customContents . "\n\n" . $lang["userrequestnotification2"] . "\n$baseurl";
+
+	send_mail($email_notify,$applicationname . ": " . $lang["requestuserlogin"] . " - " . getval("name",""),$message,"",$user_email,"","",getval("name",""));
+
+	return true;
+	}
+
 if (getval("save","")!="")
 	{
+	# Check for required fields
+
+	# Required fields (name, email) not set?
+	$missingFields = hook('replacemainrequired');
+	if (!is_array($missingFields))
+		{
+		$missingFields = array();
+		if (getval("name","")=="") { $missingFields[] = $lang["yourname"]; }
+		if (getval("email","")=="") { $missingFields[] = $lang["youremailaddress"]; }
+		}
+
+	# Add custom fields
+	$customContents="";
+	if (isset($custom_registration_fields))
+		{
+		$custom=explode(",",$custom_registration_fields);
+
+		# Required fields?
+		if (isset($custom_registration_required)) {$required=explode(",",$custom_registration_required);}
+
+		# Loop through custom fields
+		for ($n=0;$n<count($custom);$n++)
+			{
+			$custom_field_value = getval("custom" . $n,"");
+			$custom_field_sub_value_list = "";
+
+			for ($i=1; $i<=1000; $i++)		# check if there are sub values, i.e. custom<n>_<n> form fields, for example a bunch of checkboxes if custom type is set to "5"
+				{
+				$custom_field_sub_value = getval("custom" . $n . "_" . $i, "");
+				if ($custom_field_sub_value == "") continue;
+				$custom_field_sub_value_list .= ($custom_field_sub_value_list == "" ? "" : ", ") . $custom_field_sub_value;		# we have found a sub value so append to the list
+				}
+
+			if ($custom_field_sub_value_list != "")		# we found sub values
+				{
+				$customContents.=i18n_get_translated($custom[$n] . ": " . $custom_field_sub_value_list) . "\n\n";		# append with list of all sub values found
+				}
+			elseif ($custom_field_value != "")		# if no sub values found then treat as normal field
+				{
+				$customContents.=i18n_get_translated($custom[$n] . ": " . $custom_field_value) . "\n\n";		# there is a value so append it
+				}
+			elseif (isset($required) && in_array($custom[$n],$required))		# if the field was mandatory and a value or sub value(s) not set then we return false
+				{
+				$missingFields[] = $custom[$n];
+				}
+			}
+		}
+
+	if (!empty($missingFields))
+		{
+		$error=$lang["requiredfields"] . ' ' . i18n_get_translated(implode(', ', $missingFields), true);
+		}
 	# Check the anti-spam code is correct
-	if (getval("antispamcode","")!=md5(getval("antispam","")))
+	elseif (getval("antispamcode","")!=md5(getval("antispam","")))
 		{
 		$error=$lang["requiredantispam"];
 		}
-	
 	# Check that the e-mail address doesn't already exist in the system
-	elseif (user_email_exists(getval("email","")))
+	elseif (user_email_exists($user_email))
 		{
 		# E-mail already exists
-		$error=$lang["accountemailalreadyexists"];$error_extra="<br/><a href=\"".$baseurl_short."pages/user_password.php?email=" . urlencode(getval("email","")) . "\">" . $lang["forgottenpassword"] . "</a>";
+		$error=$lang["accountemailalreadyexists"];$error_extra="<br/><a href=\"".$baseurl_short."pages/user_password.php?email=" . urlencode($user_email) . "\">" . $lang["forgottenpassword"] . "</a>";
 		}
 	else
 		{
