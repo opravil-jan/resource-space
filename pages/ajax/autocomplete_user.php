d153dd7adabae511089592137e08be3c1d931523
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-03-26 22:09:48 +0000

    ADD - New optional parameter 'nogroups' to enable just retrieval of users for autocomplete_user.php

--- pages/ajax/autocomplete_user.php
+++ pages/ajax/autocomplete_user.php
@@ -7,6 +7,7 @@ include "../../include/general.php";
 
 $find=getvalescaped("term","");
 $getrefs=(getvalescaped("getrefs","")!="")?true:false;
+$ignoregroups=(getvalescaped("nogroups","")!="")?true:false;
 $first=true;
 ?> [ <?php
 $users=get_users(0,$find);
@@ -22,27 +23,28 @@ for ($n=0;$n<count($users) && $n<=20;$n++)
 		?>{ "label": "<?php echo $users[$n]["fullname"]?>", "value": "<?php echo $users[$n]["username"]?>" <?php if ($getrefs){?>,  "ref": "<?php echo $users[$n]["ref"]?>"<?php }?> } <?php
 		}
 	}
-?>
-<?php
-$groups=get_usergroups(true,$find);
-
-for ($n=0;$n<count($groups) && $n<=20;$n++)
+if(!$ignoregroups)
 	{
-	$show=true;
-	if (checkperm("E") && ($groups[$n]["ref"]!=$usergroup) && ($groups[$n]["parent"]!=$usergroup) && ($groups[$n]["ref"]!=$usergroupparent)) {$show=false;}
-	if ($show)
+	$groups=get_usergroups(true,$find);
+
+	for ($n=0;$n<count($groups) && $n<=20;$n++)
 		{
-		$users=get_users($groups[$n]["ref"]);
-		$ulist="";
-		
-		for ($m=0;$m<count($users);$m++) {if ($ulist!="") {$ulist.=", ";};$ulist.=$users[$m]["username"];}
-		if ($ulist!="")
+		$show=true;
+		if (checkperm("E") && ($groups[$n]["ref"]!=$usergroup) && ($groups[$n]["parent"]!=$usergroup) && ($groups[$n]["ref"]!=$usergroupparent)) {$show=false;}
+		if ($show)
 			{
-		    if (!$first) { ?>, <?php }
-			$first=false;
+			$users=get_users($groups[$n]["ref"]);
+			$ulist="";
 			
-			?>{ "label": "<?php echo $lang["group"]?>: <?php echo $groups[$n]["name"]?>", "value": "<?php echo $lang["group"]?>: <?php echo $groups[$n]["name"]?>" <?php if ($getrefs){?>,  "ref": "<?php echo $groups[$n]["ref"]?>"<?php }?> }<?php 
+			for ($m=0;$m<count($users);$m++) {if ($ulist!="") {$ulist.=", ";};$ulist.=$users[$m]["username"];}
+			if ($ulist!="")
+				{
+				if (!$first) { ?>, <?php }
+				$first=false;
+				
+				?>{ "label": "<?php echo $lang["group"]?>: <?php echo $groups[$n]["name"]?>", "value": "<?php echo $lang["group"]?>: <?php echo $groups[$n]["name"]?>" <?php if ($getrefs){?>,  "ref": "<?php echo $groups[$n]["ref"]?>"<?php }?> }<?php 
+				}
 			}
 		}
-	}
+		}
 ?> ]
