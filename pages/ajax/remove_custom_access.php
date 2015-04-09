b34adbe57a7d5d2284efcb1d2bdfd88224c773c4
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-04-09 08:57:51 +0000

    FIX Security patch - users need edit access in order to remove custom access to users

--- pages/ajax/remove_custom_access.php
+++ pages/ajax/remove_custom_access.php
@@ -2,9 +2,17 @@
 include dirname(__FILE__) . '/../../include/db.php';
 include dirname(__FILE__) . '/../../include/authenticate.php';
 include dirname(__FILE__) . '/../../include/general.php';
+include dirname(__FILE__) . '/../../include/resource_functions.php';
 
 $resource = getvalescaped('resource', '');
 $user_ref = getvalescaped('user_ref', '');
+
+$resource_data = get_resource_data($resource);
+
+// User should have edit access to this resource!
+if(!get_edit_access($resource, $resource_data['archive'], false, $resource_data)) {
+	exit ('Permission denied.');
+}
 
 // Delete the record from the database
 $query = sprintf('
