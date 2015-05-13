54e45532b923eba86410221fefd81cbca337f137
jethro <jethro@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-05-13 07:37:06 +0000

    FIX Responsive styling issues and compatibility with the new dash

--- plugins/rse_responsive/config/config.php
+++ plugins/rse_responsive/config/config.php
@@ -4,5 +4,5 @@ global $contact_link,$nav2contact_link;
 $nav2contact_link = ($contact_link == true || $nav2contact_link == true)? true : false;
 $responsiveheaderimgsrc="";
 $contact_link = false;
-$rcsskey=10;
+$rcsskey=11;
 ?>
