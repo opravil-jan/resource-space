508b8b255bc9de15b6299854f02896e481294f2f
jethro <jethro@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-07-06 14:53:20 +0000

    ADD manage dash page to "My Account", this allows config tiles that have been turned off to be added back onto your dash. You can also create text tiles from here.

---
+++ pages/user/user_dash_admin.php
@@ -0,0 +1,169 @@
+<?php
+
+include "../../include/db.php";
+include "../../include/general.php";
+include "../../include/authenticate.php";
+include "../../include/dash_functions.php";
+
+#If can't manage own dash return to user home.
+if(!($home_dash && checkPermission_dashmanage()))
+	{header("location: ".$baseurl_short."pages/user/user_home.php");exit;}
+
+
+#Building function
+function build_user_dash_tile_list($user)
+	{
+	global $lang;
+	#All available Tiles
+	$dtiles_available = get_user_available_tiles($user);
+	foreach($dtiles_available as $tile)
+  		{
+  		$checked = false;
+  		if(!empty($tile["dash_tile"]))
+  			{$checked=true;}
+  		?>
+  		<tr>
+  			<td>
+  				<input 
+  					type="checkbox" 
+  					class="tilecheck" 
+  					name="usertiles[]" 
+  					value="<?php echo $tile["ref"];?>" 
+  					onChange="changeTile(<?php echo $tile["ref"];?>,<?php echo $tile["all_users"];?>);"
+  					<?php echo $checked?"checked":"";?> 
+  				/>
+  			</td>
+  			<td><?php echo $tile["title"];?></td>
+  			<td><?php echo substr($tile["txt"],0,75);?></td>
+  			<td><?php echo substr($tile["link"],0,75);?></td>
+  			<td><?php echo $tile["resource_count"]? $lang["yes"]: $lang["no"];?></td>
+  			<td>
+  				
+  			</td>
+  		</tr>
+  		<?php
+  		}
+  	}
+
+if(getvalescaped("quicksave",FALSE))
+	{
+	$tile = getvalescaped("tile","");
+	#If a valid tile value supplied
+	if(!empty($tile) && is_numeric($tile))
+		{
+		#Tile available to this user?
+		$available = get_user_available_tiles($userref,$tile);
+		if(!empty($available))
+			{
+			$tile = $available[0]["tile"];
+			$usertile = $available[0]["usertile"];
+			if(get_user_tile($usertile,$userref))
+				{
+				#Delete if the user already has the tile
+				delete_user_dash_tile($usertile,$userref);
+				exit(build_user_dash_tile_list($userref));
+				}
+			else
+				{
+				#Add to the front of the pile if the user already has the tile
+				add_user_dash_tile($userref,$tile,5);
+				exit(build_user_dash_tile_list($userref));
+				}
+			}
+		}
+	exit("Save Failed");
+	}
+
+if(getvalescaped("submit",FALSE))
+	{
+	$tiles = getvalescaped("usertiles","");
+	if(empty($tiles))
+		{
+		empty_user_dash($userref);
+		}
+	else
+		{
+		#Start Fresh
+		empty_user_dash($userref,false);
+		$order_by = 10;
+		foreach($tiles as $tile)
+			{
+			add_user_dash_tile($userref,$tile,$order_by);
+			$order_by+=10;
+			}
+		}
+	}
+
+
+include "../../include/header.php";
+?>
+<div class="BasicsBox"> 
+	<h1><?php echo $lang["manage_own_dash"];?></h1>
+	<form class="Listview">
+	<input type="hidden" name="submit" value="true" />
+	<table class="ListviewStyle">
+		<thead>
+			<tr class="ListviewTitleStyle">
+				<td><?php echo $lang["dashtileshow"];?></td>
+				<td><?php echo $lang["dashtiletitle"];?></td>
+				<td><?php echo $lang["dashtiletext"];?></td>
+				<td><?php echo $lang["dashtilelink"];?></td>
+				<td><?php echo $lang["showresourcecount"];?></td>
+				<td><?php echo $lang["tools"];?></td>
+			</tr>
+		</thead>
+		<tbody id="dashtilelist">
+	  	<?php
+	  	build_user_dash_tile_list($userref);
+	  	?>
+	  </tbody>
+  	</table>
+  	<div id="confirm_dialog" style="display:none;text-align:left;"><?php echo $lang["dashtiledeleteusertile"];?></div>
+  	<noscript>
+	  	<div class="QuestionSubmit">
+	  		<input type="submit" value="<?php echo $lang["save"]?>"/>
+	  	</div>
+  	</noscript>
+	</form>
+	<script type="text/javascript">
+		function processTileChange(tile) {
+			jQuery.post(
+				window.location,
+				{"tile":tile,"quicksave":"true"},
+				function(data){
+					jQuery("#dashtilelist").html(data);
+				}
+			);
+		}
+		function changeTile(tile,all_users) {
+			if(all_users==0) {
+				jQuery("#confirm_dialog").dialog({
+		        	title:'<?php echo $lang["dashtiledelete"]; ?>',
+		        	modal: true,
+    				resizable: false,
+					dialogClass: 'confirm-dialog no-close',
+                    buttons: {
+                        "<?php echo $lang['confirmdashtiledelete'] ?>": function() {processTileChange(tile); jQuery(this).dialog( "close" );},
+                        "<?php echo $lang['cancel'] ?>":  function() { jQuery(".tilecheck[value="+tile+"]").attr('checked', true); jQuery(this).dialog('close'); }
+                    }
+                });
+			} else {
+				processTileChange(tile);
+			}
+		}
+	</script>
+	<div>
+		<?php
+		# Create New Tile (Has dtu or dta (hdta) permissions)
+		if($home_dash && checkPermission_dashmanage())
+			{ ?>
+			<p>
+				<a href="<?php echo $baseurl."/pages/dash_tile.php?create=true&tltype=ftxt&modifylink=true&freetext=Helpful%20tips%20here&nostyleoptions=true&all_users=1&link=http://resourcespace.org/knowledge-base/&title=Knowledge%20Base";?>">&gt;&nbsp; <?php echo $lang["createdashtilefreetext"]?></a>
+			</p>
+			<?php
+			} ?>
+	</div>
+</div>
+
+<?php
+include "../../include/footer.php";
--- pages/user/user_home.php
+++ pages/user/user_home.php
@@ -3,7 +3,6 @@
 include "../../include/db.php";
 include "../../include/general.php";
 include "../../include/authenticate.php";
-include "../../include/resource_functions.php";
 include "../../include/header.php";
 ?>
 
@@ -14,24 +13,22 @@ include "../../include/header.php";
   
 	<div class="VerticalNav">
 	<ul>
-	
         <li><a href="<?php echo $baseurl_short?>pages/user/user_change_password.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["changeyourpassword"]?></a></li>
-    
         <?php
       	if ($disable_languages==false && $show_language_chooser)
-		{
-		?>
-		<li><a href="<?php echo $baseurl_short?>pages/change_language.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["languageselection"]?></a></li>
-		<?php
-		}
-	?>
-	
-        <li><a href="<?php echo $baseurl_short?>pages/contribute.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["mycontributions"]?></a></li>
-
-	<li><a href="<?php echo $baseurl_short?>pages/collection_manage.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["mycollections"]?></a></li>
+			{?>
+			<li><a href="<?php echo $baseurl_short?>pages/change_language.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["languageselection"]?></a></li>
+			<?php
+			} ?>
+		<li><a href="<?php echo $baseurl_short?>pages/contribute.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["mycontributions"]?></a></li>
+		<li><a href="<?php echo $baseurl_short?>pages/collection_manage.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["mycollections"]?></a></li>
 		
-
-	
+		<?php
+		if($home_dash && checkPermission_dashmanage())
+			{ ?>
+			<li><a href="<?php echo $baseurl_short?>pages/user/user_dash_admin.php"	onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["manage_own_dash"];?></a></li>
+			<?php
+			} ?>
 	</ul>
 	</div>
 
@@ -39,4 +36,3 @@ include "../../include/header.php";
 
 <?php
 include "../../include/footer.php";
-?>
