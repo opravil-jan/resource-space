a410c92b379da5863a77a9cebf27f7084a536eb3
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-07-08 09:19:49 +0000

    RMV function from search and put it inside legacy_actions plugin

--- pages/search.php
+++ pages/search.php
@@ -747,46 +747,6 @@ if (true) # Always show search header now.
 			elseif (strpos($search,"!")!==false) {$rel=$lang["asadded"];}
 			}
 
-		function display_sort_order($name, $label)
-			{
-			global $order_by;
-			if (isset($GLOBALS['display_fields_added']))
-				echo '&nbsp;&nbsp;|&nbsp;&nbsp;';
-			else
-				$GLOBALS['display_fields_added'] = true;
-			$fixedOrder = $name=='relevance';
-			$selected = $order_by==$name;
-			if ($selected && $fixedOrder)
-				{
-				?><span class="Selected"><?php echo $label?></span><?php
-				}
-			else
-				{
-				global $baseurl_short, $revsort, $search, $archive, $restypes, $k, $sort;
-				if ($selected)
-					{
-					?><span class="Selected"><?php
-					}
-				?><a href="<?php echo $baseurl_short?>pages/search.php?search=<?php
-					echo urlencode($search) . '&amp;order_by=' . $name . '&amp;archive='
-							. urlencode($archive) . '&amp;k=' . urlencode($k) . '&amp;restypes='
-							. urlencode($restypes);
-					if ($selected)
-						{
-						echo '&amp;sort=' . urlencode($revsort);
-						}
-					?>" onClick="return CentralSpaceLoad(this);"><?php echo $label ?></a><?php
-					if (!$fixedOrder && $selected)
-						{
-						?><div class="<?php echo urlencode($sort)?>">&nbsp;</div><?php
-						}
-					if ($selected)
-						{
-						?></span><?php
-						}
-				}
-			}
-
 		$orderFields = array('relevance' => $rel);
 		if ($random_sort)
 			$orderFields['random'] = $lang['random'];
--- plugins/legacy_actions/hooks/search.php
+++ plugins/legacy_actions/hooks/search.php
@@ -1,4 +1,6 @@
 <?php
+include_once __DIR__ . '/../include/search_functions.php';
+
 function HookLegacy_actionsSearchRender_sort_order_differently($orderFields)
     {
     foreach($orderFields as $order => $label)
