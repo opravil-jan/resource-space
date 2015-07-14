896de69b1536ecab6ebd55c3aa9e887ba9a700bb
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-07-14 15:18:03 +0000

    FIX: Temporarily removing the modal loading of edit from a modal view, so that modal edits are only possible from the search page. Prevents a scenario whereby a modal edit is over a fullscreen edit, which currently won't work well due to conflicting JS variable names and IDs.

--- pages/view.php
+++ pages/view.php
@@ -1206,7 +1206,7 @@ hook ("resourceactions") ?>
 		}
 	if ($edit_access) 
 		{ ?>
-		<li><a href="<?php echo $baseurl_short?>pages/edit.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>"    onClick="return <?php echo ($modal?"Modal":"CentralSpace") ?>Load(this,true);">
+		<li><a href="<?php echo $baseurl_short?>pages/edit.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>"    onClick="return CentralSpaceLoad(this,true);">
 			<?php echo ($resourcetoolsGT?"&gt; ":"").$lang["action-edit"]?>
 		</a></li>
 		<?php 
