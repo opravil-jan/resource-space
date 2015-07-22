adec03be1fcbc6869d2f92d7f05f08648751ecf9
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-07-22 12:31:53 +0000

    FIX: Whitegrey plugin - only set the logo variable if not already set - allows custom plugins to override this setting. Will apply to other plugins after further testing.

--- plugins/col-whitegry/config/config.php
+++ plugins/col-whitegry/config/config.php
@@ -1,4 +1,4 @@
 <?php
-$linkedheaderimgsrc="/gfx/titles/title.png";
+global $baseurl_short,$linkedheaderimgsrc;
+if ($linkedheaderimgsrc=="") {$linkedheaderimgsrc=$baseurl_short . "gfx/titles/title.png";}
 $slimheader_darken=true;
-
