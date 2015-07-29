618d6ede6dea6cd9f4ca612f5658ed206ff57ecc
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-07-29 15:54:45 +0000

    CHG: convert compact tools options to actions option

--- plugins/transform/hooks/all.php
+++ plugins/transform/hooks/all.php
@@ -5,17 +5,19 @@ global $baseurl,$baseurl_short;?>
 <link rel="stylesheet" href="<?php echo $baseurl_short?>plugins/transform/lib/jcrop/css/jquery.Jcrop.min.css" type="text/css" />
 <script type="text/javascript" src="<?php echo $baseurl?>/plugins/transform/lib/jcrop/js/jquery.Jcrop.min.js" language="javascript"></script>
 <?php } 
-
-function HookTransformAllCollectiontoolcompact1($collection, $count_result,$cinfo,$colresult,$col_editable){
-	global $cropper_enable_batch,$lang;
-	global $baseurl_short;
-
-	if ($cropper_enable_batch && $count_result>0 && $col_editable){
-	?>
-	<option value="<?php echo $collection?>|0|0|<?php echo $baseurl_short?>plugins/transform/pages/collection_transform.php?collection=<?php echo $collection?>|main|false">&gt;&nbsp;<?php echo $lang["transform"]?>...</option>
-	<?php
+function HookTransformAllRender_actions_add_collection_option(){
+	global $cropper_enable_batch,$count_result,$cinfo,$lang,$collection,$baseurl_short;
+	
+	$options = '';
+	if ($cropper_enable_batch && $count_result>0 && $cinfo["allow_changes"]==1){
+		$data_attribute['url'] = sprintf('%splugins/transform/pages/collection_transform.php?collection=%s',
+            $baseurl_short,
+            urlencode($collection)
+        );
+        $options.=render_dropdown_option('transform', $lang["transform"],$data_attribute);
 	}
-    
+	
+	return $options;
 }
 function HookTransformAllAdditional_title_pages_array(){
         return array("crop","collection_transform");
