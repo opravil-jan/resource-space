b36caee3fc3d1dab7a30540d4a9cfc827b6dbf1b
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-08-04 13:32:48 +0000

    FIX missing code to include old compact collection tool in collection actions

--- plugins/flickr_theme_publish/hooks/themes.php
+++ plugins/flickr_theme_publish/hooks/themes.php
@@ -13,21 +13,27 @@ function HookFlickr_theme_publishThemesAddcustomtool($theme)
 	<?php
 	}
 
-function HookFlickr_theme_publishThemesCollectiontoolcompact1($collection, $count_result)
-    {
-    # Adds a Flickr command to the themes page in collection compact style.
-    global $getthemes, $m, $lang, $baseurl_short;
+function HookFlickr_theme_publishThemesRender_actions_add_collection_option(){
+	global $getthemes, $m, $lang, $baseurl_short;
     $theme = $getthemes[$m];
-
-    if ($count_result>0) # Don't show the option if the theme is empty.
+	$options = '';
+	// these aren't really set on themes.php
+	$result=get_collection_resources($theme['ref']);
+	$count_result=count($result);
+	
+	if ($count_result>0) # Don't show the option if the theme is empty.
         {
-
-        # Work out how many resources in this theme are unpublished.
-        $unpublished = sql_value("select count(*) value from resource join collection_resource on resource.ref=collection_resource.resource where collection_resource.collection='" . $theme["ref"] . "' and flickr_photo_id is null",0);
-
-        ?>
-        <option value="<?php echo $theme["ref"]?>|0|0|<?php echo $baseurl_short?>plugins/flickr_theme_publish/pages/sync.php?theme=<?php echo $collection?>|main|false">&gt;&nbsp;<?php echo $lang["publish_to_flickr"] ?>...<?php if ($unpublished>0) { echo " <strong>(" . ($unpublished==1 ? $lang["unpublished-1"] : str_replace("%number", $unpublished, $lang["unpublished-2"])) . ")</strong>"; } ?></option>
-        <?php
-        }
-    }
+			$lang_string=$lang["publish_to_flickr"];
+			$unpublished = sql_value("select count(*) value from resource join collection_resource on resource.ref=collection_resource.resource where collection_resource.collection='" . $theme["ref"] . "' and flickr_photo_id is null",0);
+			if ($unpublished>0) {
+				$lang_string.=" <strong>(" . ($unpublished==1 ? $lang["unpublished-1"] : str_replace("%number", $unpublished, $lang["unpublished-2"])) . ")</strong>";
+			}
+		$data_attribute['url'] = sprintf('%splugins/flickr_theme_publish/pages/sync.php?theme=%s',
+            $baseurl_short,
+            urlencode($theme["ref"])
+        );
+        $options.=render_dropdown_option('flickr_publish', $lang_string,$data_attribute);
+	}
+	return $options;
+}
 ?>
