1568f251c8e24617ff932e13a4424aa291119a9a
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2015-08-07 13:35:48 +0000

    FIX & CHG: hook modifycompacttoolslabel was removed with the migration to actions. This adds hook modifyactionslabel in the same relative location

--- include/render_functions.php
+++ include/render_functions.php
@@ -156,8 +156,14 @@ function render_actions(array $collection_data, $top_actions = true, $two_line =
         ?>
 
     <div class="ActionsContainer  <?php if($top_actions) { echo 'InpageNavLeftBlock'; } ?>">
-        <div class="DropdownActionsLabel"><?php echo $lang['actions']; ?>:</div>
-    <?php
+		<?php
+		if (!hook("modifyactionslabel"))
+			{
+			?>
+			<div class="DropdownActionsLabel"><?php echo $lang['actions']; ?>:</div>
+			<?php
+			}
+
     if($two_line)
         {
         ?>
