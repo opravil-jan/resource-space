788b8b6d51b948339ade4d4d18724f0a9cef4e1c
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-01-15 13:05:24 +0000

    FIX: tweak to language in last commit

--- languages/en.php
+++ languages/en.php
@@ -2391,5 +2391,5 @@ $lang["slideshow_use_static_image"] = "Use a single random image from the set (i
 
 $lang["emailcollectionrequest"] = "[img_gfx/titles/title.gif]<br />The following user has made a request:<br />Username: [username]<br />User email: [useremail]<br /><br />Reason for request: [requestreason]<br /><br />Click the link below to view the request.<br /><a href='[requesturl]'>[requesturl]</a>";
 $lang["emailusercollectionrequest"] = "[img_gfx/titles/title.gif]<br />Your resource request has been submitted for approval and will be looked at shortly.:<br /><br />Reason for request: [requestreason]<br /><br />Click the link below to view the requested resources.<br /><a href='[url]'>[url]</a>";
-$lang['user_pref_show_notifications'] = "Show me system notifications on screen as they are received. If set to false the counter will still update to indicate the presence of new messages";
+$lang['user_pref_show_notifications'] = "Show me system notifications on screen as they are received. If disabled, the counter will still update to indicate the presence of new messages";
 $lang['mymessages_introtext'] = "You can configure which messages appear here by changing your user preferences";
