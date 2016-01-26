f2e3b4aff62cee054f5d7fddada17ab80344df21
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-01-26 11:35:14 +0000

    CHG: Version control plugin enabled by default (can't see a reason why users would not want this).

--- include/config.default.php
+++ include/config.default.php
@@ -774,7 +774,7 @@ $user_preferences = true;
 # List of active plugins.
 # Note that multiple plugins must be specified within array() as follows:
 # $plugins=array("loader","rss","messaging","googledisplay"); 
-$plugins = array('transform');
+$plugins = array('transform', 'rse_version');
 
 # Uncomment and set the next line to allow anonymous access. 
 # You must set this to the USERNAME of the USER who will represent all your anonymous users
