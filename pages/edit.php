676f68888c1dc1489b740a1002feef201c9d4bb2
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-02-02 09:55:14 +0000

    FIX: Replacing hardcoded language string

--- pages/edit.php
+++ pages/edit.php
@@ -1621,7 +1621,7 @@ if($collapsible_sections)
     <label for="copyfrom"><?php echo $lang["batchcopyfrom"]?></label>
     <input class="stdwidth" type="text" name="copyfrom" id="copyfrom" value="" style="width:80px;">
     <input type="submit" id="copyfromsubmit" name="copyfromsubmit" value="<?php echo $lang["copy"]?>" onClick="event.preventDefault();CentralSpacePost(document.getElementById('mainform'),true);">
-    <input type="submit" name="save" value="Save">
+    <input type="submit" name="save" value="<?php echo $lang["save"];?>">
  </div><!-- end of question_copyfrom -->
  <?php
 }
