d3fcea3df52c9bd99f3e2d56446fd5d079c628d7
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-04-20 23:05:19 +0000

    NEW: Resources with missing geolocation data appear in a table using the existing InfoTable class

--- branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
+++ branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
@@ -15,6 +15,7 @@ $collectionname = $collection['name'];
 $markers = array();
 $mean_lat=0;
 $mean_long=0;
+$check = false;
 ?>
 
 <h1><?php echo $lang["geolocatecollection"] ?></h1>
@@ -22,8 +23,8 @@ $mean_long=0;
 
 <?php
 
-foreach ($all_resources as $value) {
-    
+foreach ($all_resources as $value) 
+	{
     $resource = get_resource_data($value,$cache=true);
     $forthumb = get_resource_data($resource['ref']);
     $url = get_resource_path($resource['ref'],false,"thm",$generate=true,$extension="jpg",$scramble=-1,$page=1,$watermarked=false,$file_modified="",$alternative=-1,$includemodified=true);
@@ -31,7 +32,31 @@ foreach ($all_resources as $value) {
 	$parts =  explode('?',$new);
 	if ( $resource['geo_long'] == '' || $resource['geo_lat'] == '')
 		{
-		echo $lang['location-missing'] . "&nbsp;" . $resource['ref'] . "&nbsp;"; ?><a href=<?php echo $baseurl . "/pages/geo_edit.php?ref=" . $resource['ref'] . "&nbsp;"?>><?php echo $lang['location-add']?></a>
+		if (!$check)
+			{
+			echo $lang['location-missing'];
+			//Set check to true so the text above and the table below 
+			//are only rendered if geolocation data are missing
+			$check = true;
+			?>
+			<table class="InfoTable">
+			<tr>
+			<td><?php echo $lang["resourceid"]?></td>
+			<td><?php echo $lang["action-preview"]?></td>
+			<td><?php echo $lang['location-add']?></td>
+			</td>
+			</tr>
+			<?php
+			}
+			?>
+
+		<tr>
+		<td><?php echo $resource['ref']?></td>
+		<td><a href=<?php echo $baseurl . "/pages/view.php?ref=" . $resource['ref'] ?> > <img  src=<?php echo '..' . $parts[0]?> </img></a></td>
+		<td><a href=<?php echo $baseurl . "/pages/geo_edit.php?ref=" . $resource['ref'] ?> > <?php echo $lang['location-add']?></a></td>
+		</tr>
+		
+		
 		<?php
 		}
 	else
@@ -41,12 +66,12 @@ foreach ($all_resources as $value) {
 		$mean_long=$mean_long+$resource['geo_long'];
 		$mean_lat=$mean_lat+$resource['geo_lat']; 
 		}
-}
+	}
 $mean_lat=$mean_lat/count($markers);
 $mean_long=$mean_long/count($markers);
 
 ?>
-
+<?php if ($check){?></table><?php echo "<br>";} ?>
 <div id="GeoColDiv" style="width:900px; height:450px;"></div>
   
 <script src="http://www.openlayers.org/api/OpenLayers.js"></script>
@@ -66,30 +91,28 @@ $mean_long=$mean_long/count($markers);
     var markers = <?php echo str_replace(array('"','\\'),'',json_encode($markers)) ?>;
     var paths = <?php echo str_replace('\\','',json_encode($paths)) ?>;
 	var baseurl = <?php echo str_replace('\\','',json_encode($baseurl) )?>;
-    for (var i=0; i<markers.length; i++) {
-      
-       var lon = markers[i][0];
-       var lat = markers[i][1];
-       var rf = String(markers[i][2]);
-       var width = markers[i][3];
-       var height = markers[i][4];
-       var reslink = paths[i];
+    for (var i=0; i<markers.length; i++)
+		{
+        var lon = markers[i][0];
+        var lat = markers[i][1];
+        var rf = String(markers[i][2]);
+        var width = markers[i][3];
+        var height = markers[i][4];
+        var reslink = paths[i];
        
-       var feature = new OpenLayers.Feature.Vector(
-                new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
-                {description: baseurl +  '/pages/view.php?ref=' + rf},
-                {externalGraphic: '..' + reslink, graphicHeight: height*0.5, graphicWidth: width*0.5, graphicXOffset:-32, graphicYOffset:-45 }
-            );   
+        var feature = new OpenLayers.Feature.Vector(
+			new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
+			{description: baseurl +  '/pages/view.php?ref=' + rf},
+			{externalGraphic: '..' + reslink, graphicHeight: height*0.5, graphicWidth: width*0.5, graphicXOffset:-32, graphicYOffset:-45 }
+		);   
 		      
-    
-    vectorLayer.addFeatures(feature);
-
-    vectorLayer.events.register("featureselected", null, function(event){
+		vectorLayer.addFeatures(feature);
+		
+		vectorLayer.events.register("featureselected", null, function(event){
         var layer = event.feature.layer;
         ModalLoad(event.feature.attributes.description)
         selectControl.unselectAll();
-
-    });
+		});
     
     // Add select feature control required to trigger events on the vector layer.
     var selectControl = new OpenLayers.Control.SelectFeature(vectorLayer);
