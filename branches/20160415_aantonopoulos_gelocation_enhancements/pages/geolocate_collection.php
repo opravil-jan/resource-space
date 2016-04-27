abe73ed94e64bc5c95018150d3ec046ce3ec9f92
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-04-27 01:22:19 +0000

    FIX: Reinstating accidentally removed variable from config.default.php and fixing map to include all collecion assets upon plotting

--- branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
+++ branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
@@ -5,7 +5,8 @@ include "../include/authenticate.php";
 include "../include/resource_functions.php";
 include "../include/collections_functions.php";
 include "../include/header.php";
-
+//need to honour geo_search_restrict still pending
+//The two variables below act like "pemissions" to display or not the page
 if ( $disable_geocoding || (!$disable_geocoding && !$geo_locate_collection) ){exit($lang["error-permissiondenied"]);}
 
 global $baseurl;
@@ -15,8 +16,6 @@ $all_resources = get_collection_resources($ref);
 $collection = get_collection($ref);
 $collectionname = $collection['name'];
 $markers = array();
-$mean_lat = 0;
-$mean_long = 0;
 $check = false;
 ?>
 
@@ -32,11 +31,18 @@ if ( count($all_resources) == 0 ) {  exit( $lang["geoemptycollection"]);  }
 foreach ($all_resources as $value) 
 	{
     $resource = get_resource_data($value,$cache=true);
-	//echo get_resource_access($resource['ref']);
-    $forthumb = get_resource_data($resource['ref']);
+	
+	//hide the resource if it is confidential
+	if ( get_resource_access($resource['ref'])==2 ) {continue;}
+    
+    //If the resource is not confidential keep going
+    else
+    {
+	$forthumb = get_resource_data($resource['ref']);
     $url = get_resource_path($resource['ref'],false,"thm",$generate=true,$extension="jpg",$scramble=-1,$page=1,$watermarked=false,$file_modified="",$alternative=-1,$includemodified=true);
 	$new = str_replace($baseurl,"", $url);
 	$parts =  explode('?',$new);
+	
 	if ( $resource['geo_long'] == '' || $resource['geo_lat'] == '')
 		{
 		if (!$check)
@@ -70,14 +76,10 @@ foreach ($all_resources as $value)
 		{
 		//These arrays are going to be passed to Javascript below to plot
 		$markers[] =  [ $resource['geo_long'] . "," .  $resource['geo_lat'] . "," . $resource['ref'] . "," . $forthumb['thumb_width'] . "," . $forthumb['thumb_height']];
-		$paths[] = $parts[0];
-		$mean_long=$mean_long+$resource['geo_long'];
-		$mean_lat=$mean_lat+$resource['geo_lat']; 
+		$paths[] = $parts[0]; 
 		}
 	}
-
-$mean_lat=$mean_lat/count($markers);
-$mean_long=$mean_long/count($markers);
+}
 
 ?>
 <?php if ($check){?></table><?php echo "<br>";} ?>
@@ -92,11 +94,7 @@ $mean_long=$mean_long/count($markers);
     map.addLayer(new OpenLayers.Layer.OSM('OSM'));
     epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
     projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
-    
-    var lonLat = new OpenLayers.LonLat(  <?php echo $mean_long . "," . $mean_lat;?> ).transform(epsg4326, projectTo);
-     
-    var zoom=15;
-    map.setCenter (lonLat, zoom);
+         
     var vectorLayer = new OpenLayers.Layer.Vector("Thumbnails");
     var vectorLayer2 = new OpenLayers.Layer.Vector("Markers");
     
@@ -105,6 +103,7 @@ $mean_long=$mean_long/count($markers);
     var markers = <?php echo str_replace(array('"','\\'),'',json_encode($markers)) ?>;
     var paths = <?php echo str_replace('\\','',json_encode($paths)) ?>;
 	var baseurl = <?php echo str_replace('\\','',json_encode($baseurl) )?>;
+
     for (var i=0; i<markers.length; i++)
 		{
         var lon = markers[i][0];
@@ -114,17 +113,18 @@ $mean_long=$mean_long/count($markers);
         var height = markers[i][4];
         var reslink = paths[i];
         
-		var feature = new OpenLayers.Feature.Vector(
+        var feature = new OpenLayers.Feature.Vector(
 			new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
 			{description: baseurl +  '/pages/view.php?ref=' + rf},
 			
-			{externalGraphic: '..' + reslink, graphicHeight: height*0.45, graphicWidth: width*0.45, graphicXOffset:0, graphicYOffset:0 }
-		);  
+			{externalGraphic: '..' + reslink, graphicHeight: height*0.45, graphicWidth: width*0.45 }
+		);
+		
 		var feature2 = new OpenLayers.Feature.Vector(
 			new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
 			{description: baseurl +  '/pages/view.php?ref=' + rf},
 			
-			{externalGraphic: '../lib/OpenLayers/img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:0, graphicYOffset:0 }
+			{externalGraphic: '../lib/OpenLayers/img/marker.png', graphicHeight: 25, graphicWidth: 21 }
 		);  
 		
 		vectorLayer.addFeatures(feature);
@@ -157,7 +157,8 @@ $mean_long=$mean_long/count($markers);
     
     map.addLayer(vectorLayer);
     map.addLayer(vectorLayer2);
-
+    
+    map.zoomToExtent(vectorLayer.getDataExtent());
 	
 </script>
 
