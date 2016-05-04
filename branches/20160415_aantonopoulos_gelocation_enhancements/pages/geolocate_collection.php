45602970a399715274e8d2464d7fdc5d9f11e42c
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-05-04 21:21:18 +0000

    NEW: Initial work for supporting gpx tracks, merging from trunk to keep branch up to date

--- branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
+++ branches/20160415_aantonopoulos_gelocation_enhancements/pages/geolocate_collection.php
@@ -31,19 +31,31 @@ if ( count($all_resources) == 0 ) {  exit( $lang["geoemptycollection"]);  }
 foreach ($all_resources as $value) 
 	{
     $resource = get_resource_data($value,$cache=true);
-	
+    //print_r($resource);
+	//print_r(get_resource_path($resource['ref'],false,"",$generate=true,$extension="gpx",$scramble=-1,$page=1,$watermarked=false,$file_modified="",$alternative=-1,$includemodified=false));
 	//hide the resource if it is confidential
 	if ( get_resource_access($resource['ref'])==2 ) {continue;}
     
     //If the resource is not confidential keep going
     else
     {
+	$file_extension = $resource['file_extension'];
+	//if ( $gpx==='gpx'){continue;}
 	$forthumb = get_resource_data($resource['ref']);
+	
+	if ( $file_extension ==='gpx' )
+	{
+	$url = get_resource_path($resource['ref'],false,"",$generate=true,$extension="gpx",$scramble=-1,$page=1,$watermarked=false,$file_modified="",$alternative=-1,$includemodified=false);
+	}
+	else
+	{
     $url = get_resource_path($resource['ref'],false,"thm",$generate=true,$extension="jpg",$scramble=-1,$page=1,$watermarked=false,$file_modified="",$alternative=-1,$includemodified=true);
+	}
+	
 	$new = str_replace($baseurl,"", $url);
 	$parts =  explode('?',$new);
 	
-	if ( $resource['geo_long'] == '' || $resource['geo_lat'] == '')
+	if ( ( $resource['geo_long'] == '' || $resource['geo_lat'] == '') && $file_extension !='gpx' )
 		{
 		if (!$check)
 			{
@@ -75,8 +87,12 @@ foreach ($all_resources as $value)
 	else
 		{
 		//These arrays are going to be passed to Javascript below to plot
-		$markers[] =  [ $resource['geo_long'] . "," .  $resource['geo_lat'] . "," . $resource['ref'] . "," . $forthumb['thumb_width'] . "," . $forthumb['thumb_height']];
-		$paths[] = $parts[0]; 
+		//echo $resource['field8'];
+		$markers[] =  [ $resource['geo_long'] . "," .  $resource['geo_lat'] . "," . $resource['ref'] . "," . $forthumb['thumb_width'] . "," . $forthumb['thumb_height']  ];
+		$paths[] = $parts[0];
+		$titles[] = $resource['field8'];
+		$file_extensions[] = $file_extension;
+		//print_r($markers); 
 		}
 	}
 }
@@ -85,11 +101,12 @@ foreach ($all_resources as $value)
 <?php if ($check){?></table><?php echo "<br>";} ?>
 
 <div id="GeoColDiv" style="width:900px; height:450px;"></div>
-  
+
 <script src="../lib/OpenLayers/OpenLayers.js"></script>
 <script>
 
     map = new OpenLayers.Map("GeoColDiv");
+    
     map.addControl(new OpenLayers.Control.LayerSwitcher({'ascending':false}));
     map.addLayer(new OpenLayers.Layer.OSM('OSM'));
     epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
@@ -102,6 +119,8 @@ foreach ($all_resources as $value)
     //of backslashes because Javascript was complaining
     var markers = <?php echo str_replace(array('"','\\'),'',json_encode($markers)) ?>;
     var paths = <?php echo str_replace('\\','',json_encode($paths)) ?>;
+    var titles = <?php echo json_encode($titles)?>;
+    var file_extension = <?php echo json_encode($file_extensions)?>;
 	var baseurl = <?php echo str_replace('\\','',json_encode($baseurl) )?>;
 
     for (var i=0; i<markers.length; i++)
@@ -112,6 +131,9 @@ foreach ($all_resources as $value)
         var width = markers[i][3];
         var height = markers[i][4];
         var reslink = paths[i];
+        var title = titles[i];
+        var fe = file_extension[i];
+        //alert(title);
         
         var feature = new OpenLayers.Feature.Vector(
 			new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
@@ -127,25 +149,42 @@ foreach ($all_resources as $value)
 			{externalGraphic: '../lib/OpenLayers/img/marker.png', graphicHeight: 25, graphicWidth: 21 }
 		);  
 		
+		if ( fe ==='gpx') {
+		// Add the Layer with GPX Track
+		var lgpx = new OpenLayers.Layer.Vector(title, {
+			
+			strategies: [new OpenLayers.Strategy.Fixed()],
+			protocol: new OpenLayers.Protocol.HTTP({
+				url: '..' + reslink,
+				format: new OpenLayers.Format.GPX({
+extractWaypoints: true, extractRoutes: true, extractAttributes: true, keepData:true,
+})
+			}),
+			style: {strokeColor: "blue", strokeWidth: 5, strokeOpacity: 0.9},
+			
+			projection: new OpenLayers.Projection("EPSG:4326")
+		});
+		
+		map.addLayer(lgpx);
+	}
 		vectorLayer.addFeatures(feature);
 		vectorLayer2.addFeatures(feature2);
+		
 		//Hide by default the default red marker and display
 		//the thumbnails layer
-		vectorLayer2.setVisibility(false)
-		
-	}
-	
+		vectorLayer.setVisibility(false)		
+}
 	vectorLayer.events.register("featureselected", null, function(event){
-        var layer = event.feature.layer;
         ModalLoad(event.feature.attributes.description)
         selectControl.unselectAll();
 		});
+
 	vectorLayer2.events.register("featureselected", null, function(event){
-        var layer = event.feature.layer;
         ModalLoad(event.feature.attributes.description)
         selectControl.unselectAll();
 		});
-    
+    	
+		
     // Add select feature control required to trigger events on the vector layer.
     var selectControl = new OpenLayers.Control.SelectFeature(vectorLayer);
     map.addControl(selectControl);
@@ -153,12 +192,14 @@ foreach ($all_resources as $value)
     
     var selectControl2 = new OpenLayers.Control.SelectFeature(vectorLayer2);
     map.addControl(selectControl2);
-    selectControl2.activate();   
+    selectControl2.activate();  
     
-    map.addLayer(vectorLayer);
+	
+	map.addLayer(vectorLayer);
     map.addLayer(vectorLayer2);
+	
+    map.zoomToExtent(vectorLayer2.getDataExtent());
     
-    map.zoomToExtent(vectorLayer.getDataExtent());
 	
 </script>
 
