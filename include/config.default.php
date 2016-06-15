2ddd0ce47c9b745fb263c03aa0d9342c8bde8d73
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-06-15 22:27:40 +0000

    FIX: Bringing the repo up to date, did not do that in the previous commit

--- include/config.default.php
+++ include/config.default.php
@@ -681,7 +681,7 @@ $themes_navlink = true;
 # display an alert icon next to the team centre link 
 # and the relevant team centre item when there are requests that need managing
 # only affects users with permissions to do this.
-$team_centre_alert_icon = false;
+$team_centre_alert_icon = true;
 
 # Hide mycontributions link from regular users
 $mycontributions_userlink=true;
@@ -794,24 +794,6 @@ $resource_type_icons=false;
 $defaulttheme="";
 
 
-/** DEPRECATED **/
-
-	# Theme chips available. This makes it possible to add new themes and chips using the same structure.
-	# To create a new theme, you need a chip in gfx/interface, a graphics folder called gfx/<themename>,
-	# and a css file called css/Col-<themename>.css
-	# this is a basic way of adding general custom themes that do not affect SVN checkouts, 
-	# though css can also be added in plugins as usual.
-	 
-	$available_themes=array("multi", "whitegry","greyblu","black");
-
-	# NOTE: Do not add custom themes to $available_themes_by_default.
-	# This is being used to know which themes are custom
-	$available_themes_by_default = array("multi", "whitegry","greyblu","black","slimcharcoal");
-
-	# Uncomment and set the next line to lock to one specific colour scheme (e.g. greyblu/whitegry).
-	# $userfixedtheme="whitegry";
-
-/** END OF DEPRECATED **/
 
 /** USER PREFERENCES **/
 $user_preferences = true;
@@ -2688,7 +2670,7 @@ $ctrls_to_save=false;
 # Option to separate some resource types in searchbar selection boxes
 $separate_resource_types_in_searchbar=Array();
 
-$team_centre_bug_report=true;
+$team_centre_bug_report=false;
 
 # Option to show resource archive status in search results list view
 $list_view_status_column=false;
