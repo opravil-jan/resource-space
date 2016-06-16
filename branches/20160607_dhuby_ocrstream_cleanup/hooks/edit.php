bad805324d0aed519176942eefc523016f72421c
rdamrau <rdamrau@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-06-16 08:08:20 +0000

    CHG: [OCRstream] Now use absolute path for edit.php hook.

--- branches/20160607_dhuby_ocrstream_cleanup/hooks/edit.php
+++ branches/20160607_dhuby_ocrstream_cleanup/hooks/edit.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../plugins/ocrstream/include/ocrstream_functions.php";
+require_once dirname(__DIR__) . "/include/ocrstream_functions.php";
 
 function HookOcrstreamEditEditbeforeheader()
     {
