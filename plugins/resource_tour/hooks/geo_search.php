010bc8d305b76a06e58421775f7766a0ecb079e0
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-08-02 23:22:01 +0000

    NEW: Marking search radius on map, placing user marker point, preparation fordynamically placing assets

--- plugins/resource_tour/hooks/geo_search.php
+++ plugins/resource_tour/hooks/geo_search.php
@@ -1,34 +1,39 @@
 <?php
 
+
 function HookResource_tourGeo_searchGeosearch()
 	{
 		?>
 				
-<button id="locate">Locate me!</button>
-<input id="radius" type ="range" min ="1000" max="100000" step ="50" value ="1000"/>
-
-        <input type="checkbox" name="track" id="track">
+Search radius around me [meters]:<div id="value">500</div>
+<input id="radius" type="range" min="50" max="100000" step="50" value="500" onmousemove="showrangevalue();"/>
+<button id="locate">Locate me and fetch resources!</button>
+<input type="checkbox" name="track" id="track">
         <label for="track">Follow me!</label>
 <div id="LoadingBox"></div>
-<script>
+<script type="text/javascript">
+	
+var uurl = "<?php global $baseurl;echo $baseurl;?>"+"/plugins/resource_tour/ajax/nearby_resources.php";
 
-//epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+function showrangevalue(){
+  document.getElementById("value").innerHTML=document.getElementById("radius").value;
+}
+
 projectTo = map.getProjectionObject(); 
 var vector = new OpenLayers.Layer.Vector('Resource Tour!');
 map.addLayers([vector]);
 
 
-		var geolocate = new OpenLayers.Control.Geolocate({
-			bind: false,
-			geolocationOptions: {
-				enableHighAccuracy: false,
-				maximumAge: 0,
-				timeout: 5000
-			}
-		});
+var geolocate = new OpenLayers.Control.Geolocate({
+	bind: false,
+	geolocationOptions: {
+		enableHighAccuracy: false,
+		maximumAge: 0,
+		timeout: 5000
+	}
+});
 
 map.addControl(geolocate);
-var firstGeolocation = true;
 
 geolocate.events.register("locationupdated",geolocate,function(e) {
 		var radius 	    = parseFloat(document.getElementById('radius').value);
@@ -38,7 +43,9 @@ geolocate.events.register("locationupdated",geolocate,function(e) {
 		var MyPos       = new OpenLayers.Geometry.Point(e.point.x, e.point.y).transform(mercator,geographic);
 		var x = parseFloat(e.point.x);
 		var y = parseFloat(e.point.y);
-		
+		//Slightly perturb user's point, can not use identical points for some reason 
+		var MyPosPer    = new OpenLayers.Geometry.Point(x+0.000001, e.point.y);
+		//alert(MyPosPer);
 		var Bound_South = new OpenLayers.Geometry.Point(x, y-radius).transform(mercator,geographic);
 		var Bound_North = new OpenLayers.Geometry.Point(x, y+radius).transform(mercator,geographic);
 		
@@ -51,54 +58,59 @@ geolocate.events.register("locationupdated",geolocate,function(e) {
 		
 		//var retVal = null; 
 		
-				function ajaxCallBack(data){
-					var markers = data;
-					return markers;
-				}
+		function ajaxCallBack(data){
+			var markers = data;
+			return markers;
+		}
 
-				jQuery('#LoadingBox').show();
-				  jQuery.ajax({
-				  type:'POST',
-				  url: 'https://localhost/resourcespace/rs/branches/Geo/plugins/resource_tour/ajax/nearby_resources.php',            
-				  dataType: 'json',				  
-				  data: {
-						jsonData: JSON.stringify({
-							"coord": MyPos,"Bound_East":Bound_East,"Bound_West":Bound_West,"Bound_North":Bound_North,"Bound_South":Bound_South,
-						})} ,
-				  
-				  success: function(data)          
-				  {
-					//alert(data);
-					ajaxCallBack(data);
-					alert(data);
-					jQuery('#LoadingBox').hide();
-					var me = new OpenLayers.Feature.Vector(
-						new OpenLayers.Geometry.Point( e.point.x, e.point.y ).transform(geographic, projectTo)
-						);
-					
-					vector.addFeatures(new OpenLayers.Feature.Vector(
-						e.point
-						),
-						me
-						);
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					map.zoomToExtent(vector.getDataExtent());
+		jQuery('#LoadingBox').show();
+		jQuery.ajax({
+		type:'POST',
+		url:uurl,
+		//url: 'https://localhost/resourcespace/rs/branches/Geo/plugins/resource_tour/ajax/nearby_resources.php',            
+		dataType: 'json',				  
+		data: {
+			  jsonData: JSON.stringify({
+				  "coord": MyPos,"Bound_East":Bound_East,"Bound_West":Bound_West,"Bound_North":Bound_North,"Bound_South":Bound_South,
+			  })} ,
+		
+		success: function(data)          
+		{
+			//alert(data);
+			vector.removeAllFeatures();
+			ajaxCallBack(data);
+			//alert(data);
+			jQuery('#LoadingBox').hide();
+			//var me = new OpenLayers.Geometry.Point( e.point.x, e.point.y ).transform(geographic, projectTo);
+			//alert(e.point.x);
+			var me = new OpenLayers.Feature.Vector(
+					e.point,
+					{},
+					{	externalGraphic:'../plugins/resource_tour/gfx/marker.png', graphicHeight: 25, graphicWidth: 15
+						//externalGraphic: '../lib/OpenLayers/img/marker.png', graphicHeight: 25, graphicWidth: 21 ,
 						
-				  }
-				});
-			  
-			}
+					}
+				);
 			
-			);
+		
+			var circle = new OpenLayers.Feature.Vector(
+				new  OpenLayers.Geometry.Polygon.createRegularPolygon( MyPosPer ,radius, 50,0),null,{fillColor: '#ea2828',
+				strokeColor: '#ea2828',
+				strokeOpacity: 1,
+				strokeWidth: 4,
+				graphicZIndex: 1099,
+				fillOpacity: 0});
+			
+			vector.addFeatures([circle,me]);
+			
+			map.zoomToExtent(vector.getDataExtent());
+			  
+		}
+		});
+	  
+	}
+	
+	);
 
 
 geolocate.events.register("locationfailed",this,function() {
@@ -107,9 +119,8 @@ geolocate.events.register("locationfailed",this,function() {
 
 
 document.getElementById('locate').onclick = function() {
-    vector.removeAllFeatures();
     geolocate.deactivate();
-    firstGeolocation = true;
+    //firstGeolocation = true;
     geolocate.activate();
 };
 
